5bfd40a929465fa302fe552e6ef375bc0e54a798
Aaron Harsh <aaron.harsh@gmail.com>
2020-03-29 23:08:37 -0700

    checkpoint

--- tests/envs/test_simple_cribbage_env.py
+++ tests/envs/test_simple_cribbage_env.py
@@ -28,50 +28,50 @@ class TestSimpleCribbageEnv(unittest.TestCase):
         _, player_id = env.step(action)
         self.assertEqual(player_id, env.game.round.current_player)
 
-    # def test_step_back(self):
-    #     env = rlcard.make('uno', config={'allow_step_back':True})
-    #     state, player_id = env.init_game()
-    #     action = np.random.choice(state['legal_actions'])
-    #     env.step(action)
-    #     env.step_back()
-    #     self.assertEqual(env.game.round.current_player, player_id)
+    def test_step_back(self):
+        env = rlcard.make('simple-cribbage', config={'allow_step_back':True})
+        state, player_id = env.init_game()
+        action = np.random.choice(state['legal_actions'])
+        env.step(action)
+        env.step_back()
+        self.assertEqual(env.game.round.current_player, player_id)
 
-    #     env = rlcard.make('uno', config={'allow_step_back':False})
-    #     state, player_id = env.init_game()
-    #     action = np.random.choice(state['legal_actions'])
-    #     env.step(action)
-    #     # env.step_back()
-    #     self.assertRaises(Exception, env.step_back)
+        env = rlcard.make('simple-cribbage', config={'allow_step_back':False})
+        state, player_id = env.init_game()
+        action = np.random.choice(state['legal_actions'])
+        env.step(action)
+        # env.step_back()
+        self.assertRaises(Exception, env.step_back)
 
-    # def test_run(self):
-    #     env = rlcard.make('uno')
-    #     env.set_agents([RandomAgent(env.action_num) for _ in range(env.player_num)])
-    #     trajectories, payoffs = env.run(is_training=False)
-    #     self.assertEqual(len(trajectories), 2)
-    #     total = 0
-    #     for payoff in payoffs:
-    #         total += payoff
-    #     self.assertEqual(total, 0)
-    #     trajectories, payoffs = env.run(is_training=True, seed=1)
-    #     total = 0
-    #     for payoff in payoffs:
-    #         total += payoff
-    #     self.assertEqual(total, 0)
+    def test_run(self):
+        env = rlcard.make('simple-cribbage')
+        env.set_agents([RandomAgent(env.action_num) for _ in range(env.player_num)])
+        trajectories, payoffs = env.run(is_training=False)
+        self.assertEqual(len(trajectories), 2)
+        total = 0
+        for payoff in payoffs:
+            total += payoff
+        self.assertEqual(total, 0)
+        trajectories, payoffs = env.run(is_training=True, seed=1)
+        total = 0
+        for payoff in payoffs:
+            total += payoff
+        self.assertEqual(total, 0)
 
-    # def test_decode_action(self):
-    #     env = rlcard.make('uno')
-    #     env.init_game()
-    #     legal_actions = env._get_legal_actions()
-    #     for legal_action in legal_actions:
-    #         decoded = env._decode_action(legal_action)
-    #         self.assertLessEqual(decoded, ACTION_LIST[legal_action])
+    def test_decode_action(self):
+        env = rlcard.make('simple-cribbage')
+        env.init_game()
+        legal_actions = env._get_legal_actions()
+        for legal_action in legal_actions:
+            decoded = env._decode_action(legal_action)
+            self.assertLessEqual(decoded, ACTION_LIST[legal_action])
 
-    # def test_single_agent_mode(self):
-    #     env = rlcard.make('uno', config={'single_agent_mode':True})
-    #     state = env.reset()
-    #     self.assertIsInstance(state, dict)
-    #     for _ in range(100):
-    #         state, _, _ = env.step(np.random.choice(state['legal_actions']))
+    def test_single_agent_mode(self):
+        env = rlcard.make('simple-cribbage', config={'single_agent_mode':True})
+        state = env.reset()
+        self.assertIsInstance(state, dict)
+        for _ in range(100):
+            state, _, _ = env.step(np.random.choice(state['legal_actions']))
 
 if __name__ == '__main__':
     unittest.main()
