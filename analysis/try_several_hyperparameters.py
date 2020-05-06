1813609fcd6b43697d179ce18e570260b62ee0a9
Aaron Harsh <aaron.harsh@gmail.com>
2020-05-05 17:38:47 -0700

    Include activation function as a hyperparameter

--- analysis/try_several_hyperparameters.py
+++ analysis/try_several_hyperparameters.py
@@ -6,27 +6,29 @@ import pathlib
 episodes = 200000
 
 output_dir = 'logs'
+
+activations = ['relu', 'tanh']
+
 rl_rates = [0.001, 0.005, 0.0005, 0.0001, 0.01, 0.0002, 0.0008]
 
 layerses = [
-    '1024,1024,1024,1024,1024',
-    '1024,1024,1024,512,256,128',
-    '1024,1024,512,256,128',
-    '1024,1024,1024,512,256,128',
-    '1024,1024,1024,128',
-    '1024,1024,1024,1024,128',
+    '128,128',
+    '64,128,64',
+    '512,1024,2048,1024,512',
     '2048,1024,512,256,128',
-    '2048,2048,2048,256,128',
+    '2048,1024,1024,512,256,128'
     ]
 
 pathlib.Path(output_dir).mkdir(exist_ok=True)
 
-for rl_rate in rl_rates:
-    print(f"rl_rate {rl_rate}")
-    for layers in layerses:
-        print(f"layers {layers}")
-        output_file = output_dir + "/output-" + str(rl_rate) + "-" + layers
-        if not pathlib.Path(output_file).exists():
-            command = f"PYTHONPATH=. python examples/simple_cribbage_nfsp.py --episodes={episodes} --layers={layers} --rl-rate={rl_rate} > {output_file}"
-            print(command)
-            os.system(command)
+for activation in activations:
+    print(f"activation {activation}")
+    for rl_rate in rl_rates:
+        print(f"rl_rate {rl_rate}")
+        for layers in layerses:
+            print(f"layers {layers}")
+            output_file = f"{output_dir}/output-{activation}-{rl_rate}-{layers}"
+            if not pathlib.Path(output_file).exists():
+                command = f"PYTHONPATH=. python examples/simple_cribbage_nfsp.py --episodes={episodes} --layers={layers} --rl-rate={rl_rate} --activation={activation} > {output_file}"
+                print(command)
+                os.system(command)
