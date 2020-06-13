7bdf2da0c4a04ceb66e43c635bac6151db367ead
Aaron Harsh <aaron.harsh@gmail.com>
2020-06-13 14:58:23 -0700

    Try different droupout_keeps first

--- analysis/try_several_hyperparameters.py
+++ analysis/try_several_hyperparameters.py
@@ -23,13 +23,14 @@ layerses = [
 
 pathlib.Path(output_dir).mkdir(exist_ok=True)
 
-for activation in activations:
-    print(f"activation {activation}")
-    for rl_rate in rl_rates:
-        print(f"rl_rate {rl_rate}")
-        for layers in layerses:
-            print(f"layers {layers}")
-            for dropout_keep in dropout_keeps:
+for dropout_keep in dropout_keeps:
+    print(f"dropout_keep {dropout_keep}")
+    for activation in activations:
+        print(f"activation {activation}")
+        for rl_rate in rl_rates:
+            print(f"rl_rate {rl_rate}")
+            for layers in layerses:
+                print(f"layers {layers}")
                 output_file = f"{output_dir}/output-{activation}-dropout={dropout_keep}-{rl_rate}-{layers}"
                 if not pathlib.Path(output_file).exists():
                     command = f"PYTHONPATH=. python examples/simple_cribbage_nfsp.py --episodes={episodes} --layers={layers} --rl-rate={rl_rate} --activation={activation} --dropout={dropout_keep} > {output_file}"
